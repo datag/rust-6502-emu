c6a40bf4bc23345287d7af13f4009263011380de
Dominik D. Geyer <dominik.geyer@gmail.com>
2023-07-16 15:51:26 +0200

    memory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use crate::cpu::Cpu;
+use crate::mem::Memory;
 
 pub mod cpu;
+pub mod mem;
 
 fn main() {
     println!("rust-6502-emu");
 
+    let mem = Memory::create();
     let cpu = Cpu::create();
     println!("CPU values: {:?}", cpu);
 }
---
+++ src/mem.rs
@@ -0,0 +1,13 @@
+const MEMORY_SIZE: usize = 0xffff;
+
+pub struct Memory {
+    pub data: [u8; MEMORY_SIZE]
+}
+
+impl Memory {
+    pub fn create() -> Memory {
+        Memory {
+            data: [0; MEMORY_SIZE]
+        }
+    }
+}
