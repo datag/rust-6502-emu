912b2d93b13044164c8b6ea98b27636440b4ddf2
Dominik D. Geyer <dominik.geyer@gmail.com>
2023-07-16 22:33:26 +0200

    begin instructions

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,20 +1,20 @@
-#[derive(Debug)]
-pub enum InstructionEnum {
-    // ADC - Add Memory to Accumulator with Carry
-    ADC_IMM,
-    ADC_ZPG,
-    ADC_ZPX,
-    ADC_ABS,
-    ADC_ABX,
-    ADC_ABY,
-    ADC_IDX,
-    ADC_IDY,
-}
+// #[derive(Debug)]
+// pub enum InstructionEnum {
+//     // ADC - Add Memory to Accumulator with Carry
+//     ADC_IMM,
+//     ADC_ZPG,
+//     ADC_ZPX,
+//     ADC_ABS,
+//     ADC_ABX,
+//     ADC_ABY,
+//     ADC_IDX,
+//     ADC_IDY,
+// }
 
+pub const ADC_IMM: u8 = 0x69;
 
 #[derive(Debug)]
 pub struct Instruction {
-    pub ins_enum: InstructionEnum,
     pub opcode: u8,
     pub bytes: u8,
     pub cycles: u8,
@@ -24,7 +24,7 @@ pub struct Instruction {
 impl Instruction {
     pub fn from_opcode(opcode: u8) -> Instruction {
         match opcode {
-            0x69 => Instruction { ins_enum: InstructionEnum::ADC_IMM, opcode: 0x69, bytes: 2, cycles: 2 },
+            ADC_IMM => Instruction { opcode: 0x69, bytes: 2, cycles: 2 },
             _ => panic!("Unimplemented or invalid instruction {:2X}", opcode),
         }
     }
