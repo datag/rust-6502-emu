09dc5159fa4af489f36ea0be7cbbd664f5fa9c55
Dominik D. Geyer <dominik.geyer@gmail.com>
2023-07-16 22:57:42 +0200

    ADC opcode

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,33 +1,46 @@
 use std::fmt;
 
+// ADC - Add with Carry
 pub const ADC_IMM: u8 = 0x69;
+pub const ADC_ZPG: u8 = 0x65;
+pub const ADC_ZPX: u8 = 0x75;
+pub const ADC_ABS: u8 = 0x6D;
+pub const ADC_ABX: u8 = 0x7D;
+pub const ADC_ABY: u8 = 0x79;
+pub const ADC_IDX: u8 = 0x61;
+pub const ADC_IDY: u8 = 0x71;
 
-pub struct Instruction {
+pub struct Instruction<'a> {
     pub opcode: u8,
+    pub mnemonic: &'a str,
     pub bytes: u8,
     pub cycles: u8,
 }
 
+impl Instruction<'_> {
+    pub fn from_opcode(opcode: u8) -> Instruction<'static> {
 
-impl Instruction {
-    pub fn from_opcode(opcode: u8) -> Instruction {
-        let mut ins: Instruction;
-        
         match opcode {
-            ADC_IMM => ins = Instruction { opcode: 0x69, bytes: 2, cycles: 2 },
+            // ADC
+            ADC_IMM => Instruction { opcode: ADC_IMM, mnemonic: "ADC", bytes: 2, cycles: 2 },
+            ADC_ZPG => Instruction { opcode: ADC_ZPG, mnemonic: "ADC", bytes: 2, cycles: 3 },
+            ADC_ZPX => Instruction { opcode: ADC_ZPX, mnemonic: "ADC", bytes: 2, cycles: 4 },
+            ADC_ABS => Instruction { opcode: ADC_ABS, mnemonic: "ADC", bytes: 3, cycles: 4 },
+            ADC_ABX => Instruction { opcode: ADC_ABX, mnemonic: "ADC", bytes: 3, cycles: 4 /* +1 if page crossed */ },
+            ADC_ABY => Instruction { opcode: ADC_ABY, mnemonic: "ADC", bytes: 3, cycles: 4 /* +1 if page crossed */ },
+            ADC_IDX => Instruction { opcode: ADC_IDX, mnemonic: "ADC", bytes: 2, cycles: 6 },
+            ADC_IDY => Instruction { opcode: ADC_IDY, mnemonic: "ADC", bytes: 2, cycles: 5 /* +1 if page crossed */ },
+
             _ => panic!("Unimplemented or invalid instruction {:2X}", opcode),
         }
-
-        // for all
-        ins.opcode = opcode;
-        return ins;
     }
 }
 
-impl fmt::Debug for Instruction {
+impl fmt::Debug for Instruction<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Instruction")
             .field("Opcode", &format!("{:02X}", self.opcode))
+            .field("Mnemonic", &self.mnemonic)
             .field("Bytes", &self.bytes)
             .field("Cycles", &self.cycles)
             .finish()
