b9a9ae493cd06962af41b3a3d10bd86740a5714c
Dominik D. Geyer <dominik.geyer@gmail.com>
2023-07-16 15:29:42 +0200

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use crate::cpu::create_cpu;
+
+pub mod cpu;
+
 fn main() {
-    println!("Hello, world!");
+    println!("rust-6502-emu");
+
+    let cpu = create_cpu();
+    println!("CPU values: {:?}", cpu);
 }
